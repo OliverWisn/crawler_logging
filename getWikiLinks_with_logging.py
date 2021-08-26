c95fa9f9e56482a4248c2e505a41c3e89e7fa99b
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2021-08-26 09:23:14 +0200

    Add logging to the script getWikiLinks_with_logging.py
    Add logging to the script getWikiLinks_with_logging.py to show one
    way to debug.

--- getWikiLinks_with_logging.py
+++ getWikiLinks_with_logging.py
@@ -9,8 +9,47 @@ import datetime
 import random
 import re
 import sys
+import logging
 
 from bs4 import BeautifulSoup
+
+
+    # logging.basicConfig(filename='example.log', encoding='utf-8'), 
+    #   level=logging.DEBUG)
+
+# create logger
+logger = logging.getLogger('Mainstream')
+logger.setLevel(logging.DEBUG)
+
+# create file handler which logs even debug messages
+fh = logging.FileHandler('events.log')
+fh.setLevel(logging.DEBUG)
+
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)-12s - %(levelname)-8s \
+    - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+    # format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+    #                    datefmt='%m-%d %H:%M',
+    # formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s 
+    #   - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+ch.setFormatter(formatter)
+fh.setFormatter(formatter)
+
+# add the handlers to logger
+logger.addHandler(ch)
+logger.addHandler(fh)
+
+# 'application' code
+    # logging.basicConfig(filename='example.log', encoding='utf-8')
+logger.debug('debug message')
+logger.info('info message')
+logger.warning('warn message')
+logger.error('error message')
+logger.critical('critical message')
 
 
 def getTitle(articleUrl):
@@ -86,5 +125,6 @@ while len(links) > 0:
             pages.add(newArticle)
             links = getLinks(newArticle)
         else:
+            logger.info(f"This site has already been checked: {newArticle}")
             newArticle = checkofurl
             links = getLinks(newArticle)
